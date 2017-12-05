9bd226a99113e28f513f3afbe680cbffe9e1e575
Jonathan Wu <jzw8@cornell.edu>
2017-12-05 16:08:24 -0500

    modified inotify script

--- test_inotify.py
+++ test_inotify.py
@@ -12,8 +12,12 @@ i.add_watch(dir_to_monitor)
 for event in i.event_gen():
 	if event is not None:
 		(_, type_names, path, filename) = event
+		print(type_names)
 		if(type_names[0]=='IN_CREATE'):
+			print("Encrypting file: " + filename)
 			os.system("./tea_omp")
+			print("Removing file: " + filename)
+			os.system("rm " + path+'/'+filename)
 			break
 		
 os.system("python test_inotify.py")
